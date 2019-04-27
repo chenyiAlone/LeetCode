c561e5b8135a32f797f28fb0570ce91a8125642f
chenyiAlone <1193642781@qq.com>
2019-04-27 11:18:03 +0800

    idea

--- src/hard/MedianofTwoSortedArrays.java
+++ src/hard/MedianofTwoSortedArrays.java
@@ -6,44 +6,44 @@ package hard;
  * Example 1:
  *      nums1 = [1, 3]
  *      nums2 = [2]
- *      
+ *
  *      The median is 2.0
- * 
+ *
  * Example 2:
  *      nums1 = [1, 2]
  *      nums2 = [3, 4]
- *      
+ *
  *      The median is (2 + 3)/2 = 2.5
- *      
+ *
  * @author chenyiAlone
  *
  */
 class MedianofTwoSortedArrays {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int len1 = nums1.length;
-        int len2 = nums2.length;
-    	int mid = (len1 + len2) / 2;   // Ê¹ÓÃÁËÒ»¸ö(len1 + len1) / 2´óĞ¡µÄÊı×éÀ´×÷ÎªÁÙÊ±Êı×é£¬ÓÃÓÚ²éÕÒÖĞÎ»Êı
-    	int[] array = new int[mid + 1];
-    	for (int i  = 0, j = 0, k = 0; i < array.length; i++) {    // ¹é²¢ÅÅĞò
-    		if   	(j == len1) { array[i] = nums2[k++]; }
-    		else if (k == len2) { array[i] = nums1[j++]; }
-    		else if (nums1[j] > nums2[k]) { array[i] = nums2[k++]; }
-    		else { array[i] = nums1[j++]; }
-    	}
+	private double findMedianSortedArrays(int[] nums1, int[] nums2) {
+		int len1 = nums1.length;
+		int len2 = nums2.length;
+		int mid = (len1 + len2) / 2;   // ä½¿ç”¨äº†ä¸€ä¸ª(len1 + len1) / 2å¤§å°çš„æ•°ç»„æ¥ä½œä¸ºä¸´æ—¶æ•°ç»„ï¼Œç”¨äºæŸ¥æ‰¾ä¸­ä½æ•°
+		int[] array = new int[mid + 1];
+		for (int i  = 0, j = 0, k = 0; i < array.length; i++) {    // å½’å¹¶æ’åº
+			if   	(j == len1) { array[i] = nums2[k++]; }
+			else if (k == len2) { array[i] = nums1[j++]; }
+			else if (nums1[j] > nums2[k]) { array[i] = nums2[k++]; }
+			else { array[i] = nums1[j++]; }
+		}
 //    	for (int i : array) {
 //    	    System.out.print(i + " ");
 //    	}
 //    	System.out.println();
 //    	System.out.println("mid = " + mid + "  (len1 + len2) % 2 = " + (len1+len2) % 2);
-    	if ((len1+len2) % 2 == 0) {
-    		return (array[mid] + array[mid - 1]) / 2.0; 
-    	}
-    	return array[mid];
-    }
-    public static void main(String[] args) {
-        MedianofTwoSortedArrays s = new MedianofTwoSortedArrays();
-        int[] nums1 = {1, 4};
-        int[] nums2 = {2, 3};
-        System.out.println(s.findMedianSortedArrays(nums1, nums2));
-    }
+		if ((len1+len2) % 2 == 0) {
+			return (array[mid] + array[mid - 1]) / 2.0;
+		}
+		return array[mid];
+	}
+	public static void main(String[] args) {
+		MedianofTwoSortedArrays s = new MedianofTwoSortedArrays();
+		int[] nums1 = {1, 4};
+		int[] nums2 = {2, 3};
+		System.out.println(s.findMedianSortedArrays(nums1, nums2));
+	}
 }
