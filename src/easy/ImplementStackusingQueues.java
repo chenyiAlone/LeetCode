7bb2ef59c990d8a4b33f627199aabba946059cea
chenyiAlone <1193642781@qq.com>
2019-05-13 17:14:45 +0800

    No.225	Implement Stack using Queues [update]

--- src/easy/ImplementStackusingQueues.java
+++ src/easy/ImplementStackusingQueues.java
@@ -1,62 +1,118 @@
 package easy;
 
 import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 
  * ClassName: ImplementStackusingQueues
  * @author chenyiAlone  
  * Create Time: 2019/02/10 20:34:07
  * Description: No.225
- * 
+ *
+ * ------------ update -------------
+ * 思路:
+ *
+ *      更正了以前的题解，之前的题解使用了临时数组来保存从 `queue` 中 `poll()` 的值
+ *      每次 `offer(x)` 的值只需要向后移动 `queue.size() - 1` 个位置
+ *      (因为这里的 queue已经将 `x` 入队了，所以移动未加入之前的 `size` 即可)
+ *
+ *
  * Implement the following operations of a stack using queues.
-    
-    push(x) -- Push element x onto stack.
-    pop() -- Removes the element on top of the stack.
-    top() -- Get the top element.
-    empty() -- Return whether the stack is empty.
-    Example:
-    
-    MyStack stack = new MyStack();
-    
-    stack.push(1);
-    stack.push(2);  
-    stack.top();   // returns 2
-    stack.pop();   // returns 2
-    stack.empty(); // returns false
-    Notes:
-    
-    You must use only standard operations of a queue -- which means only push to back, peek/pop from front, size, and is empty operations are valid.
-    Depending on your language, queue may not be supported natively. You may simulate a queue by using a list or deque (double-ended queue), as long as you use only standard operations of a queue.
-You may assume that all operations are valid (for example, no pop or top operations will be called on an empty stack).
+ *
+ * push(x) -- Push element x onto stack.
+ * pop() -- Removes the element on top of the stack.
+ * top() -- Get the top element.
+ * empty() -- Return whether the stack is empty.
+ * Example:
+ *
+ * MyStack stack = new MyStack();
+ *
+ * stack.push(1);
+ * stack.push(2);
+ * stack.top();   // returns 2
+ * stack.pop();   // returns 2
+ * stack.empty(); // returns false
+ * Notes:
+ *
+ * You must use only standard operations of a queue -- which means only push to back, peek/pop from front, size, and is empty operations are valid.
+ * Depending on your language, queue may not be supported natively. You may simulate a queue by using a list or deque (double-ended queue), as long as you use only standard operations of a queue.
+ * You may assume that all operations are valid (for example, no pop or top operations will be called on an empty stack).
+ *
+ *
+ *
  */
 public class ImplementStackusingQueues {
-    LinkedList<Integer> queue;
+    private Queue<Integer> queue;
     /** Initialize your data structure here. */
     public ImplementStackusingQueues() {
         queue = new LinkedList<>();
     }
-    
+
     /** Push element x onto stack. */
     public void push(int x) {
-        queue.add(x);
+        queue.offer(x);
         for (int i = 0; i < queue.size() - 1; i++) {
-            queue.add(queue.poll());
+            queue.offer(queue.poll());
         }
     }
-    
+
     /** Removes the element on top of the stack and returns that element. */
     public int pop() {
         return queue.poll();
     }
-    
+
     /** Get the top element. */
     public int top() {
         return queue.peek();
     }
-    
+
     /** Returns whether the stack is empty. */
     public boolean empty() {
         return queue.isEmpty();
+    }
+
+
+    private class ImplementStackusingQueuesOld {
+        LinkedList<Integer> queue;
+
+        /**
+         * Initialize your data structure here.
+         */
+        public ImplementStackusingQueuesOld() {
+            queue = new LinkedList<>();
+        }
+
+        /**
+         * Push element x onto stack.
+         */
+        public void push(int x) {
+            queue.add(x);
+            for (int i = 0; i < queue.size() - 1; i++) {
+                queue.add(queue.poll());
+            }
+        }
+
+        /**
+         * Removes the element on top of the stack and returns that element.
+         */
+        public int pop() {
+            return queue.poll();
+        }
+
+        /**
+         * Get the top element.
+         */
+        public int top() {
+            return queue.peek();
+        }
+
+        /**
+         * Returns whether the stack is empty.
+         */
+        public boolean empty() {
+            return queue.isEmpty();
+        }
     }
 }
 
