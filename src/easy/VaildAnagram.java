f7f448759de20585d617f8ceb95bbcde1cfb25aa
chenyiAlone <1193642781@qq.com>
2019-05-15 17:13:26 +0800

    No.242	Valid Anagram

--- src/easy/VaildAnagram.java
+++ src/easy/VaildAnagram.java
@@ -5,21 +5,49 @@ package easy;
  * @author chenyiAlone  
  * Create Time: 2019/03/09 19:06:43
  * Description: No.242
+ *
+ * --------------- update -----------------
+ * update time: 2019/5/15
+ *
+ * 思路:
+ *      更新原来的算法，只需要在每次 count -= 1 之后判断这个值是不是小于 0 了即可，不需要重新遍历一遍数组
+ *
+ *
  * Given two strings s and t , write a function to determine if t is an anagram of s.
-    
-    Example 1:
-    
-    Input: s = "anagram", t = "nagaram"
-    Output: true
-    Example 2:
-    
-    Input: s = "rat", t = "car"
-    Output: false
-    Note:
-    You may assume the string contains only lowercase alphabets.
+ *
+ * Example 1:
+ *
+ * Input: s = "anagram", t = "nagaram"
+ * Output: true
+ * Example 2:
+ *
+ * Input: s = "rat", t = "car"
+ * Output: false
+ * Note:
+ * You may assume the string contains only lowercase alphabets.
+ *
+ * Follow up:
+ * What if the inputs contain unicode characters? How would you adapt your solution to such case?
+ *
+ *
  */
 public class VaildAnagram {
     public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+        int[] count = new int[256];
+        for (char c : s.toCharArray()) {
+            count[c]++;
+        }
+        for (char c : t.toCharArray()) {
+            count[c]--;
+            if (count[c] < 0)
+                return false;
+        }
+        return true;
+    }
+
+    public boolean isAnagramOld(String s, String t) {
         int[] map = new int[256];
         for (char c : s.toCharArray()) {
             map[c]++;
