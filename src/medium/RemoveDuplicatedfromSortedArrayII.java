e2c73eb55da918dc7a15624202c03cc7f6e2e678
chenyiAlone <1193642781@qq.com>
2019-06-04 18:01:08 +0800

    No.80 Remove Duplicates from Sorted Array II

--- src/medium/RemoveDuplicatedfromSortedArrayII.java
+++ src/medium/RemoveDuplicatedfromSortedArrayII.java
@@ -5,60 +5,58 @@ package medium;
  * Create Time: 2019/4/28 11:13
  * Description: No.81
  * 思路:
- *     ps:[2,5,6,0,0,1,2] 数组随机旋转，原数组的起点就是这里的 nums[3] = 0 > nums[oldStart];
- *
- *     1. 数组是在一个位置打乱的，根据 nums[mid] 和 nums[lo] 、nums[hi] 大小关系分为两部分
- *         1). nums[mid] < nums[hi] || nums[mid] < nums[lo] , (oldStart 和 lo 可能重合)
- *                 对应 { nums[lo], ... , nums[oldStart], ..., nums[mid], ..., nums[hi] }
- *                 lo 右移的唯一情况是 target < nums[mid] && target  <= nums[hi]
- *
- *         2). nums[mid] > nums[lo] || nums[mid] > nums[hi] , (oldStart 和 hi 可能重合)
- *                 对应 { nums[lo], ... , nums[mid], ... , nums[oldStart], ... , nums[hi] }
- *                 hi 左移的唯一情况是 nums[mid] < target && nums[lo] <= target
+ *      当 j > 0 && nums[i] == nums[j] && nums[j] == nums[j - 1] 时，跳过当前元素
+ *      则 !(j > 0 && nums[i] == nums[j] && nums[j] == nums[j - 1]) 即可
  *
  *
- * Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
  *
- * (i.e., [0,0,1,2,2,5,6] might become [2,5,6,0,0,1,2]).
  *
- * You are given a target value to search. If found in the array return true, otherwise return false.
+ * Given a sorted array nums, remove the duplicates in-place such that duplicates appeared at most twice and return the new length.
+ *
+ * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
  *
  * Example 1:
  *
- * Input: nums = [2,5,6,0,0,1,2], target = 0
- * Output: true
+ * Given nums = [1,1,1,2,2,3],
+ *
+ * Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
+ *
+ * It doesn't matter what you leave beyond the returned length.
  * Example 2:
  *
- * Input: nums = [2,5,6,0,0,1,2], target = 3
- * Output: false
- * Follow up:
+ * Given nums = [0,0,1,1,1,1,2,3,3],
  *
- * This is a follow up problem to Search in Rotated Sorted Array, where nums may contain duplicates.
- * Would this affect the run-time complexity? How and why?
+ * Your function should return length = 7, with the first seven elements of nums being modified to 0, 0, 1, 1, 2, 3 and 3 respectively.
  *
+ * It doesn't matter what values are set beyond the returned length.
+ * Clarification:
+ *
+ * Confused why the returned value is an integer but your answer is an array?
+ *
+ * Note that the input array is passed in by reference, which means modification to the input array will be known to the caller as well.
+ *
+ * Internally you can think of this:
+ *
+ * // nums is passed in by reference. (i.e., without making a copy)
+ * int len = removeDuplicates(nums);
+ *
+ * // any modification to nums in your function would be known by the caller.
+ * // using the length returned by your function, it prints the first len elements.
+ * for (int i = 0; i < len; i++) {
+ *     print(nums[i]);
+ * }
  */
 public class RemoveDuplicatedfromSortedArrayII {
-    public boolean search(int[] nums, int target) {
-        int lo = 0, hi = nums.length - 1;
-        while (lo <= hi) {
-            int mid = (lo + hi) >> 1;
-            if (nums[mid] == target)
-                return true;
-            if (nums[mid] < nums[hi] || nums[mid] < nums[lo]) {
-                if (target > nums[mid] && target <= nums[hi])
-                    lo = mid + 1;
-                else
-                    hi = mid - 1;
-
-            } else if (nums[mid] > nums[lo] || nums[mid] > nums[hi]) {
-                if (target < nums[mid] && target >= nums[lo])
-                    hi = mid - 1;
-                else
-                    lo = mid + 1;
-            } else {
-                hi--;
+    public int removeDuplicates(int[] nums) {
+        if (nums.length < 2)
+            return nums.length;
+        int i = 1, j = 0;
+        while (i < nums.length) {
+            if (!(j > 0 && nums[i] == nums[j] && nums[j] == nums[j - 1])) {
+                nums[++j] = nums[i];
             }
+            i++;
         }
-        return false;
+        return j + 1;
     }
 }
--- src/medium/SearchinRotatedSortedArrayII.java
+++ src/medium/SearchinRotatedSortedArrayII.java
@@ -6,13 +6,27 @@ package medium;
  * Create Time: 2019/04/09 18:53:31
  * Description: No.81
  * 思路：
+ * 一、
  *     1. 判断 mid 落在了那个区间
  *     2. 区间的种类
  *          [2, 5, 6, 0, 0, 1, 2]
  *           l                 h
  *           |  m  |                 hi: target < A[mid] && target >= A[lo]
  *                    |   m    |     lo: target > A[mid] && target <= A[hi]
- *                    
+ *
+ *
+ *
+ * 二、
+ *
+ *     1. 数组是在一个位置打乱的，根据 nums[mid] 和 nums[lo] 、nums[hi] 大小关系分为两部分
+ *         1). nums[mid] < nums[hi] || nums[mid] < nums[lo] , (oldStart 和 lo 可能重合)
+ *                 对应 { nums[lo], ... , nums[oldStart], ..., nums[mid], ..., nums[hi] }
+ *                 lo 右移的唯一情况是 target < nums[mid] && target  <= nums[hi]
+ *
+ *         2). nums[mid] > nums[lo] || nums[mid] > nums[hi] , (oldStart 和 hi 可能重合)
+ *                 对应 { nums[lo], ... , nums[mid], ... , nums[oldStart], ... , nums[hi] }
+ *                 hi 左移的唯一情况是 nums[mid] < target && nums[lo] <= target
+ *
  * Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
     
     (i.e., [0,0,1,2,2,5,6] might become [2,5,6,0,0,1,2]).
@@ -56,4 +70,28 @@ public class SearchinRotatedSortedArrayII {
          }
          return false;
      }
+
+    public boolean search2(int[] nums, int target) {
+        int lo = 0, hi = nums.length - 1;
+        while (lo <= hi) {
+            int mid = (lo + hi) >> 1;
+            if (nums[mid] == target)
+                return true;
+            if (nums[mid] < nums[hi] || nums[mid] < nums[lo]) {
+                if (target > nums[mid] && target <= nums[hi])
+                    lo = mid + 1;
+                else
+                    hi = mid - 1;
+
+            } else if (nums[mid] > nums[lo] || nums[mid] > nums[hi]) {
+                if (target < nums[mid] && target >= nums[lo])
+                    hi = mid - 1;
+                else
+                    lo = mid + 1;
+            } else {
+                hi--;
+            }
+        }
+        return false;
+    }
  }
