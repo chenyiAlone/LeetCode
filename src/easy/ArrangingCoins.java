59f75caff53f824dee0d4a9cbc716299a31caccd
chenyiAlone <1193642781@qq.com>
2019-08-28 19:23:44 +0800

    No.441 Arranging Coins binary search

--- src/easy/ArrangingCoins.java
+++ src/easy/ArrangingCoins.java
@@ -4,7 +4,8 @@ package easy;
  * Author: chenyiAlone
  * Create Time: 2019/8/22 21:56
  * Description: No.441 Arranging Coins
- *
+ * 思路:
+ *      1. 二分，查找第一个 (1 + i) * (i / 2) 值小于等于 n 的值
  *
  * You have a total of n coins that you want to form in a staircase shape, where every k-th row must have exactly k coins.
  *
@@ -43,4 +44,22 @@ public class ArrangingCoins {
     public int arrangeCoins(int n) {
         return (int)((Math.sqrt(8.0 * n + 1) - 1) / 2);
     }
+
+    public int arrangeCoinsBinarySearch(int n) {
+        long l = 0, r = Integer.MAX_VALUE;
+        while (l <= r) {
+            long mid = (r + l) >> 1;
+            double total = (1 + mid) * (mid / 2.0);
+            if (total > n) {
+                r = mid - 1;
+            } else if (total == n) {
+                return (int)mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return (int)r;
+    }
+
 }
+
