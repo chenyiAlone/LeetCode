f2833cafc581add3ad432858074c9839c3f89c8d
chenyiAlone <1193642781@qq.com>
2019-12-14 18:53:04 +0800

    No.628 Maximum Product of Three Numbers - O(N)

--- src/easy/MaximumProductofThreeNumbers.java
+++ src/easy/MaximumProductofThreeNumbers.java
@@ -32,6 +32,25 @@ import java.util.Arrays;
 public class MaximumProductofThreeNumbers {
 
     public int maximumProduct(int[] nums) {
+        int[] max = {-1, -1, -1}, min = {-1, -1};
+        for (int i = 0; i < nums.length; i++) {
+            if (max[0] == -1 || nums[i] > nums[max[0]]) {
+                max[2] = max[1];    max[1] = max[0];    max[0] = i;
+            } else if (max[1] == -1 || nums[i] > nums[max[1]]) {
+                max[2] = max[1];    max[1] = i;
+            } else if (max[2] == -1 || nums[i] > nums[max[2]]) {
+                max[2] = i;
+            }
+            if (min[0] == -1 || nums[i] < nums[min[0]]) {
+                min[1] = min[0];    min[0] = i;
+            } else if (min[1] == -1 || nums[i] < nums[min[1]]) {
+                min[1] = i;
+            }
+        }
+        return Math.max(nums[min[0]] * nums[min[1]] * nums[max[0]], nums[max[0]] * nums[max[1]] * nums[max[2]]);
+    }
+
+    public int maximumProductNLogN(int[] nums) {
         Arrays.sort(nums);
         int ret = Integer.MIN_VALUE;
         ret = Math.max(ret, nums[nums.length - 1] * nums[nums.length - 2] * nums[nums.length - 3]);
@@ -39,5 +58,6 @@ public class MaximumProductofThreeNumbers {
         ret = Math.max(ret, nums[0] * nums[1] * nums[nums.length - 1]);
         return ret;
     }
+
 
 }
